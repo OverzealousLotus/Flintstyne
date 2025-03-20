6b196aa88c05966535e87377b1ee83a8db3ccbc0
OverzealousLotus <OverzealousLotus@proton.me>
2025-03-20 16:15:18 -0700

    Remove unnecessary files.

--- server_scripts/delight.js
+++
@@ -1,7 +0,0 @@
-// priority: 0
-// requires: farmersdelight
-ServerEvents.recipes(recipes => {
-  recipes.remove({ id: 'farmersdelight:carrot_crate' });
-  recipes.remove({ id: 'farmersdelight:potato_crate' });
-  recipes.remove({ id: 'farmersdelight:beetroot_crate' });
-})
