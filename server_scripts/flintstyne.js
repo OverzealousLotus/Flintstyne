38056fcd03b350426a7a2385f2db3ecc3799d33a
OverzealousLotus <OverzealousLotus@proton.me>
2025-03-25 14:56:24 -0700

    Change Rough Spinel to use JS instead of JSON

--- server_scripts/flintstyne.js
+++ server_scripts/flintstyne.js
@@ -1,4 +1,5 @@
 // priority: 0
+const MC_CHESTS = 'minecraft:chests/';
 ServerEvents.recipes(recipes => {
   recipes.shaped(Item.of('minecraft:ancient_debris', 1),
     [
@@ -22,6 +23,13 @@ ServerEvents.tags('item', tags => {
 });
 
 LootJS.modifiers(loot => {
+  let CNC_TABLES = [
+    'desert_pyramid', 'abandoned_mineshaft', 'jungle_temple', 'end_city_treasure',
+    'buried_treasure', 'nether_bridge', 'shipwreck_treasure', 'simple_dungeon',
+    'stronghold_corridor', 'stronghold_crossing', 'village/village_temple',
+    'village/village_toolsmith', 'village/village/weaponsmith', 'woodland_mansion'
+  ];
+  CNC_TABLES.forEach((table, index) => CNC_TABLES[index] = MC_CHESTS + table);
   loot.addBlockLootModifier('flintstyne:deepslate_ancient_ore').removeLoot(Ingredient.all)
     .addLoot([
       LootEntry.of('flintstyne:small_ancient_shard'),
@@ -38,4 +46,45 @@ LootJS.modifiers(loot => {
       LootEntry.of('flintstyne:rough_spinel_shard').when(rand => rand.randomChance(0.25))
     ]
   );
+  loot.addLootTableModifier(CNC_TABLES).removeLoot('caverns_and_chasms:silver_ingot');
+});
+
+ServerEvents.genericLootTables(tables => {
+  tables.addJson('spelunkery:gameplay/grindstone_polishing/rough_spinel', {
+    pools: [
+      {
+        rolls: 1,
+        bonus_rolls: 0,
+        entries: [
+          {
+            type: "minecraft:item",
+            name: "caverns_and_chasms:spinel"
+          }
+        ]
+      },
+      {
+        rolls: {
+          min: 2,
+          max: 4
+        },
+        bonus_rolls: 0,
+        entries: [
+          {
+            type: "minecraft:item",
+            name: "minecraft:experience_bottle"
+          }
+        ]
+      },
+      {
+        rolls: 1,
+        bonus_rolls: 0,
+        entries: [
+          {
+            type: "minecraft:item",
+            name: "minecraft:bedrock"
+          }
+        ]
+      }
+    ]
+  });
 });
