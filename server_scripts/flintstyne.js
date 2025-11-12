3fa39e0eafd24ab2dc885db3c279ea3a6bb3bae6
OverzealousLotus <OverzealousLotus@proton.me>
2025-11-11 18:31:12 -0800

    Migrate Ancient Ore to Glintstyne

--- server_scripts/flintstyne.js
+++ server_scripts/flintstyne.js
@@ -1,56 +1,72 @@
 // priority: 0
-const MC_CHESTS = 'minecraft:chests/';
-ServerEvents.recipes(recipes => {
-  recipes.shaped(Item.of('minecraft:ancient_debris', 1),
-    [
-      'SS',
-      'SS'
-    ],
-    {
-      S: 'flintstyne:large_ancient_shard'
-    }
+const MC_CHESTS = "minecraft:chests/";
+ServerEvents.recipes((recipes) => {
+  recipes.shapeless("caverns_and_chasms:spinel", "9x flintstyne:spinel_shard");
+  recipes.shapeless(
+    "flintstyne:rough_spinel",
+    "9x flintstyne:rough_spinel_shard",
   );
-  recipes.shapeless('minecraft:ancient_debris', '9x flintstyne:small_ancient_shard');
-  recipes.shapeless('caverns_and_chasms:spinel', '9x flintstyne:spinel_shard');
-  recipes.shapeless('flintstyne:rough_spinel', '9x flintstyne:rough_spinel_shard');
-  recipes.shapeless('flintstyne:rough_spinel_block', '9x flintstyne:rough_spinel');
-  recipes.shapeless('9x flintstyne:rough_spinel', 'flintstyne:rough_spinel_block')
-  recipes.shapeless('9x flintstyne:rough_spinel_shard', 'flintstyne:rough_spinel')
+  recipes.shapeless(
+    "flintstyne:rough_spinel_block",
+    "9x flintstyne:rough_spinel",
+  );
+  recipes.shapeless(
+    "9x flintstyne:rough_spinel",
+    "flintstyne:rough_spinel_block",
+  );
+  recipes.shapeless(
+    "9x flintstyne:rough_spinel_shard",
+    "flintstyne:rough_spinel",
+  );
 });
 
-ServerEvents.tags('item', tags => {
-  tags.add('spelunkery:grindable', 'flintstyne:rough_spinel');
+ServerEvents.tags("item", (tags) => {
+  tags.add("spelunkery:grindable", "flintstyne:rough_spinel");
 });
 
-LootJS.modifiers(loot => {
+LootJS.modifiers((loot) => {
   let CNC_TABLES = [
-    'desert_pyramid', 'abandoned_mineshaft', 'jungle_temple', 'end_city_treasure',
-    'buried_treasure', 'nether_bridge', 'shipwreck_treasure', 'simple_dungeon',
-    'stronghold_corridor', 'stronghold_crossing', 'village/village_temple',
-    'village/village_toolsmith', 'village/village/weaponsmith', 'woodland_mansion'
+    "desert_pyramid",
+    "abandoned_mineshaft",
+    "jungle_temple",
+    "end_city_treasure",
+    "buried_treasure",
+    "nether_bridge",
+    "shipwreck_treasure",
+    "simple_dungeon",
+    "stronghold_corridor",
+    "stronghold_crossing",
+    "village/village_temple",
+    "village/village_toolsmith",
+    "village/village/weaponsmith",
+    "woodland_mansion",
   ];
-  CNC_TABLES.forEach((table, index) => CNC_TABLES[index] = MC_CHESTS + table);
-  loot.addBlockLootModifier('flintstyne:deepslate_ancient_ore').removeLoot(Ingredient.all)
+  CNC_TABLES.forEach((table, index) => (CNC_TABLES[index] = MC_CHESTS + table));
+  loot
+    .addBlockLootModifier([
+      "caverns_and_chasms:spinel_ore",
+      "caverns_and_chasms:deepslate_spinel_ore",
+    ])
+    .removeLoot(Ingredient.all)
     .addLoot([
-      LootEntry.of('flintstyne:small_ancient_shard'),
-      LootEntry.of('flintstyne:small_ancient_shard').when(rand => rand.randomChance(0.35)),
-      LootEntry.of('flintstyne:small_ancient_shard').when(rand => rand.randomChance(0.35)),
-      LootEntry.of('flintstyne:large_ancient_shard').when(rand => rand.randomChance(0.25))
-    ]
-  );
-  loot.addBlockLootModifier(['caverns_and_chasms:spinel_ore', 'caverns_and_chasms:deepslate_spinel_ore']).removeLoot(Ingredient.all)
-    .addLoot([
-      LootEntry.of('flintstyne:rough_spinel'),
-      LootEntry.of('flintstyne:rough_spinel_shard').when(rand => rand.randomChance(0.35)),
-      LootEntry.of('flintstyne:rough_spinel_shard').when(rand => rand.randomChance(0.35)),
-      LootEntry.of('flintstyne:rough_spinel_shard').when(rand => rand.randomChance(0.25))
-    ]
-  );
-  loot.addLootTableModifier(CNC_TABLES).removeLoot('caverns_and_chasms:silver_ingot');
+      LootEntry.of("flintstyne:rough_spinel"),
+      LootEntry.of("flintstyne:rough_spinel_shard").when((rand) =>
+        rand.randomChance(0.35),
+      ),
+      LootEntry.of("flintstyne:rough_spinel_shard").when((rand) =>
+        rand.randomChance(0.35),
+      ),
+      LootEntry.of("flintstyne:rough_spinel_shard").when((rand) =>
+        rand.randomChance(0.25),
+      ),
+    ]);
+  loot
+    .addLootTableModifier(CNC_TABLES)
+    .removeLoot("caverns_and_chasms:silver_ingot");
 });
 
-ServerEvents.genericLootTables(tables => {
-  tables.addJson('spelunkery:gameplay/grindstone_polishing/rough_spinel', {
+ServerEvents.genericLootTables((tables) => {
+  tables.addJson("spelunkery:gameplay/grindstone_polishing/rough_spinel", {
     pools: [
       {
         rolls: 1,
@@ -58,22 +74,22 @@ ServerEvents.genericLootTables(tables => {
         entries: [
           {
             type: "minecraft:item",
-            name: "caverns_and_chasms:spinel"
-          }
-        ]
+            name: "caverns_and_chasms:spinel",
+          },
+        ],
       },
       {
         rolls: {
           min: 2,
-          max: 4
+          max: 4,
         },
         bonus_rolls: 0,
         entries: [
           {
             type: "minecraft:item",
-            name: "minecraft:experience_bottle"
-          }
-        ]
+            name: "minecraft:experience_bottle",
+          },
+        ],
       },
       {
         rolls: 1,
@@ -81,23 +97,25 @@ ServerEvents.genericLootTables(tables => {
         entries: [
           {
             type: "minecraft:item",
-            name: "minecraft:bedrock"
-          }
-        ]
-      }
-    ]
+            name: "minecraft:bedrock",
+          },
+        ],
+      },
+    ],
   });
 });
 
-PlayerEvents.loggedIn(event => {
-  if (!event.player.stages.has('new_join')) {
-    event.player.stages.add('new_join');
+PlayerEvents.loggedIn((event) => {
+  if (!event.player.stages.has("new_join")) {
+    event.player.stages.add("new_join");
     event.server.runCommandSilent(`give ${event.entity.username} stone_sword`);
     event.server.runCommandSilent(`give ${event.entity.username} bread 5`);
-    event.server.runCommandSilent(`give ${event.entity.username} firstaid:bandage 7`);
-    event.entity.setItemSlot(5, 'minecraft:leather_helmet');
-    event.entity.setItemSlot(4, 'minecraft:leather_chestplate');
-    event.entity.setItemSlot(3, 'minecraft:leather_leggings');
-    event.entity.setItemSlot(2, 'minecraft:leather_boots');
+    event.server.runCommandSilent(
+      `give ${event.entity.username} firstaid:bandage 7`,
+    );
+    event.entity.setItemSlot(5, "minecraft:leather_helmet");
+    event.entity.setItemSlot(4, "minecraft:leather_chestplate");
+    event.entity.setItemSlot(3, "minecraft:leather_leggings");
+    event.entity.setItemSlot(2, "minecraft:leather_boots");
   }
 });
--- startup_scripts/flintstyne.js
+++ startup_scripts/flintstyne.js
@@ -1,36 +1,39 @@
 // priority: 0
 
-StartupEvents.registry('block', blocks => {
-  blocks.create('flintstyne:deepslate_ancient_ore').displayName('Deepslate Ancient Ore')
-    .textureAll('flintstyne:block/deepslate_ancient_ore')
+StartupEvents.registry("block", (blocks) => {
+  blocks
+    .create("flintstyne:rough_spinel_block")
+    .displayName("Rough Spinel Block")
+    .textureAll("flintstyne:block/rough_spinel_block")
     .soundType(SoundType.DEEPSLATE)
-    .requiresTool(true).hardness(30).resistance(1200)
-    .tagBlock('minecraft:needs_diamond_tool').tagBlock('minecraft:mineable/pickaxe');
-  blocks.create('flintstyne:rough_spinel_block').displayName('Rough Spinel Block')
-    .textureAll('flintstyne:block/rough_spinel_block')
-    .soundType(SoundType.DEEPSLATE)
-    .requiresTool(true).hardness(30).resistance(1200)
-    .tagBlock('minecraft:needs_diamond_tool').tagBlock('minecraft:mineable/pickaxe');
+    .requiresTool(true)
+    .hardness(30)
+    .resistance(1200)
+    .tagBlock("minecraft:needs_diamond_tool")
+    .tagBlock("minecraft:mineable/pickaxe");
 });
 
-StartupEvents.registry('item', items => {
-  items.create('flintstyne:small_ancient_shard').displayName('Small Ancient Shard')
-    .texture('flintstyne:item/small_ancient_shard');
-  items.create('flintstyne:large_ancient_shard').displayName('Large Ancient Shard')
-    .texture('flintstyne:item/large_ancient_shard');
-  items.create('flintstyne:rough_spinel').displayName('Rough Spinel')
-    .texture('flintstyne:item/rough_spinel')
-  items.create('flintstyne:rough_spinel_shard').displayName('Rough Spinel Shard')
-  .texture('flintstyne:item/rough_spinel_shard')
-  items.create('flintstyne:spinel_shard').displayName('Spinel Shard')
-  .texture('flintstyne:item/spinel_shard')
+StartupEvents.registry("item", (items) => {
+  items
+    .create("flintstyne:rough_spinel")
+    .displayName("Rough Spinel")
+    .texture("flintstyne:item/rough_spinel");
+  items
+    .create("flintstyne:rough_spinel_shard")
+    .displayName("Rough Spinel Shard")
+    .texture("flintstyne:item/rough_spinel_shard");
+  items
+    .create("flintstyne:spinel_shard")
+    .displayName("Spinel Shard")
+    .texture("flintstyne:item/spinel_shard");
 });
 
-StartupEvents.registry('fluid', fluids => {
-  fluids.create('flintstyne:molten_mochite')
-  .color(0xE2BDEC)
-  .bucketColor(0xE2BDEC)
-  .stillTexture('tconstruct:fluid/molten/still')
-  .flowingTexture('tconstruct:fluid/molten/flowing')
-  .displayName('Molten Mochite');
-})
+StartupEvents.registry("fluid", (fluids) => {
+  fluids
+    .create("flintstyne:molten_mochite")
+    .color(0xe2bdec)
+    .bucketColor(0xe2bdec)
+    .stillTexture("tconstruct:fluid/molten/still")
+    .flowingTexture("tconstruct:fluid/molten/flowing")
+    .displayName("Molten Mochite");
+});
