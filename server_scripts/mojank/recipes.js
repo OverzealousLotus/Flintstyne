e38e7a17b7d320aca5b0c0b475bc90b978a1bf20
OverzealousLotus <OverzealousLotus@proton.me>
2026-02-17 19:32:18 -0800

    Use Tinkers' gear instead of vanilla

---
+++ server_scripts/mojank/recipes.js
@@ -0,0 +1,98 @@
+// priority: 0
+
+const MATERIALS = ['iron', 'gold', 'diamond', 'netherite'];
+
+/**
+ * @param {Internal.RecipesEventJS} recipes Recipe handler.
+ * Flintstyne makes use of Tinkers' gear and its systems.
+*/
+function lockVanillaGear(recipes) {
+  MATERIALS.forEach(material => {
+    recipes.remove([
+      'minecraft:' + material + '_pickaxe',
+      'minecraft:' + material + '_axe',
+      'minecraft:' + material + '_sword',
+      'minecraft:' + material + '_hoe',
+      'minecraft:' + material + '_shovel',
+    ])
+  });
+
+  MATERIALS.forEach(material => {
+    recipes.remove([
+      'minecraft:' + material + '_helmet',
+      'minecraft:' + material + '_chestplate',
+      'minecraft:' + material + '_leggings',
+      'minecraft:' + material + '_boots',
+    ])
+  });
+}
+
+ServerEvents.recipes(recipes => {
+  function bulkClear(output, input, isGem) {
+    if (isGem) {
+      recipes.remove({
+        type: 'minecraft:blasting',
+        mod: 'minecraft',
+        output: 'minecraft:' + output
+      });
+      recipes.remove({
+        type: 'minecraft:smelting',
+        mod: 'minecraft',
+        output: 'minecraft:' + output
+      });
+    } else {
+      recipes.remove({
+        type: 'minecraft:blasting',
+        mod: 'minecraft',
+        output: 'minecraft:' + output,
+        not: { input: 'minecraft:' + input }
+      });
+      recipes.remove({
+        type: 'minecraft:smelting',
+        mod: 'minecraft',
+        output: 'minecraft:' + output,
+        not: { input: 'minecraft:' + input }
+      });
+    }
+    
+  }
+  recipes.remove([
+    'minecraft:dye_blue_wool',
+    'minecraft:dye_light_blue_wool',
+    'minecraft:dye_red_wool',
+    'minecraft:dye_yellow_wool',
+    'minecraft:dye_green_wool',
+    'minecraft:dye_cyan_wool',
+    'minecraft:dye_pink_wool',
+    'minecraft:dye_white_wool',
+    'minecraft:dye_black_wool',
+    'minecraft:dye_magenta_wool',
+    'minecraft:dye_brown_wool',
+    'minecraft:dye_orange_wool',
+    'minecraft:dye_lime_wool',
+    'minecraft:dye_gray_wool',
+    'minecraft:dye_light_gray_wool',
+    'minecraft:dye_purple_wool',
+  ]);
+
+  bulkClear('iron_ingot', 'raw_iron', false);
+  bulkClear('gold_ingot', 'raw_gold', false);
+  bulkClear('copper_ingot', 'raw_copper', false);
+  bulkClear('diamond', '', true);
+  bulkClear('emerald', '', true);
+  bulkClear('lapis_lazuli', '', true);
+
+  recipes.remove([
+    'minecraft:end_crystal',
+    'minecraft:beacon',
+    'minecraft:lead',
+    'minecraft:magma_cream',
+    'minecraft:cake',
+    'minecraft:blue_dye',
+    'minecraft:sticky_piston',
+    'minecraft:daylight_detector',
+    'minecraft:crafting_table'
+  ]);
+
+  lockPickaxes(recipes);
+});
